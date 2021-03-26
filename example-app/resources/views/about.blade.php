a5db79a9b83ddd634c621ac9db683f57fddab89a
olegopro <evilgazz@gmail.com>
2021-03-26 20:40:13 +0300

    Сделал homepage и review. Сделал контроллер для записи отзывов в БВ

---
+++ example-app/resources/views/about.blade.php
@@ -0,0 +1,14 @@
+@extends('layout')
+
+@section('title') Страница про нас @endsection
+
+@section('main_content')
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="jumbotron bg-warning">
+        <div class="container">
+            <h1 class="display-3">О нас</h1>
+            <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+            <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more »</a></p>
+        </div>
+    </div>
+@endsection
